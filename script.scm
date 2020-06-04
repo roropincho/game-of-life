0f27b47df97f25ba12c517333107b60b536cacaf
roropincho <rpainchaud@DESKTOP-2IFH6BS.localdomain>
2020-06-04 15:20:07 -0400

    Changing camel case variable and function names for hyphenates names

--- script.scm
+++ script.scm
@@ -5,230 +5,230 @@
 (declare
  (extended-bindings))
 
-(define nbCol 25)
-(define nbRow 25)
+(define nb-col 25)
+(define nb-row 25)
 
-(define alive (make-vector nbRow))
-(define around (make-vector nbRow))
-(define oldAround (make-vector nbRow))
+(define alive (make-vector nb-row))
+(define around (make-vector nb-row))
+(define old-around (make-vector nb-row))
 
-(define gameOn #f)
-(define idGoBtn "go-btn")
-(define idStopBtn "stop-btn")
+(define game-on #f)
+(define id-go-btn "go-btn")
+(define id-stop-btn "stop-btn")
 
-(define (makeId x y)
-  (let ((txtX (number->string x))
-        (txtY (number->string y)))
+(define (make-id x y)
+  (let ((txt-x (number->string x))
+        (txt-y (number->string y)))
     (string-append "x"
-                   (if (null? txtX)
+                   (if (null? txt-x)
                        "_"
-                       txtX)
+                       txt-x)
                    "y"
-                   (if (null? txtY)
+                   (if (null? txt-y)
                        "_"
-                       txtY))))
+                       txt-y))))
 
-(define gameOfLife
+(define game-of-life
   (lambda ()
-    (define (transferRow i)
-      (if (< i nbRow)
+    (define (transfer-row i)
+      (if (< i nb-row)
           (begin
-            (vector-set! oldAround i (vector-copy (vector-ref around i)))
-            (transferRow (+ i 1)))))
-    (define (colsAround x y difCol difRow wasAlive)
-      (if (<= difCol 1)
+            (vector-set! old-around i (vector-copy (vector-ref around i)))
+            (transfer-row (+ i 1)))))
+    (define (cols-around x y dif-col dif-row was-alive)
+      (if (<= dif-col 1)
           (begin
-            (if (not (and (equal? difCol 0)
-                          (equal? difRow 0)))
-                (let ((indX (modulo (+ x difCol nbCol) nbCol))
-                      (indY (modulo (+ y difRow nbRow) nbRow)))
-                  (let ((oldNb (vector-ref (vector-ref around indY) indX)))
-                    (vector-set! (vector-ref around indY) indX (+ oldNb (if wasAlive
+            (if (not (and (equal? dif-col 0)
+                          (equal? dif-row 0)))
+                (let ((ind-x (modulo (+ x dif-col nb-col) nb-col))
+                      (ind-y (modulo (+ y dif-row nb-row) nb-row)))
+                  (let ((old-nb (vector-ref (vector-ref around ind-y) ind-x)))
+                    (vector-set! (vector-ref around ind-y) ind-x (+ old-nb (if was-alive
                                                                             (max -1
-                                                                                 (- 0 oldNb))
+                                                                                 (- 0 old-nb))
                                                                             1))))))
-            (colsAround x y (+ difCol 1) difRow wasAlive))))
-    (define (rowsAround x y difRow wasAlive)
-      (if (<= difRow 1)
+            (cols-around x y (+ dif-col 1) dif-row was-alive))))
+    (define (rows-around x y dif-row was-alive)
+      (if (<= dif-row 1)
           (begin
-            (colsAround x y -1 difRow wasAlive)
-            (rowsAround x y (+ difRow 1) wasAlive))))
-    (define (iterateOnRow i noROw)
-      (if (< i nbCol)
+            (cols-around x y -1 dif-row was-alive)
+            (rows-around x y (+ dif-row 1) was-alive))))
+    (define (iterate-on-row i no-row)
+      (if (< i nb-col)
           (begin
-            (let ((nbAround (vector-ref (vector-ref oldAround noROw) i))
-                  (wasAlive (vector-ref (vector-ref alive noROw) i)))
-              (let ((isAlive (or (equal? nbAround
+            (let ((nb-around (vector-ref (vector-ref old-around no-row) i))
+                  (was-alive (vector-ref (vector-ref alive no-row) i)))
+              (let ((is-alive (or (equal? nb-around
                                          3)
-                                 (and (equal? nbAround
+                                 (and (equal? nb-around
                                               2)
-                                      wasAlive))))
-                (vector-set! (vector-ref alive noROw) i isAlive)
-                (setAttribute (getElementById (makeId i noROw))
+                                      was-alive))))
+                (vector-set! (vector-ref alive no-row) i is-alive)
+                (set-attribute (get-element-by-id (make-id i no-row))
                               "class"
-                              (if isAlive
+                              (if is-alive
                                   "alive"
                                   "dead"))
-                (if (not (equal? wasAlive isAlive))
-                    (rowsAround i noROw -1 wasAlive))))
-            (iterateOnRow (+ i 1) noROw))))
-    (define (iterateOnArray i)
-      (if (< i nbRow)
+                (if (not (equal? was-alive is-alive))
+                    (rows-around i no-row -1 was-alive))))
+            (iterate-on-row (+ i 1) no-row))))
+    (define (iterate-on-array i)
+      (if (< i nb-row)
           (begin
-            (iterateOnRow 0 i)
-            (iterateOnArray (+ i 1)))))
-    (define (resetGrid)
-      (let ((cellList (querySelectorAll "td")))
-        (let ((listLength (jsObj->scmObj (##inline-host-expression "(@1@).length;" cellList))))
+            (iterate-on-row 0 i)
+            (iterate-on-array (+ i 1)))))
+    (define (reset-grid)
+      (let ((cell-list (query-selector-all "td")))
+        (let ((list-length (js->scm (##inline-host-expression "(@1@).length;" cell-list))))
           ((lambda (lst size)
-             (define (lstLoop i)
+             (define (lst-loop i)
                (if (< i size)
                    (begin
-                     (removeAttribute (##inline-host-expression "(@1@)[@2@];" lst (scmObj->jsObj i))
+                     (remove-attribute (##inline-host-expression "(@1@)[@2@];" lst (scm->js i))
                                       "class")
-                     (lstLoop (+ i 1)))))
-             (lstLoop 0))
-           cellList
-           listLength))))
-    (if gameOn
+                     (lst-loop (+ i 1)))))
+             (lst-loop 0))
+           cell-list
+           list-length))))
+    (if game-on
         (begin
-          (customTimeout)
-          (transferRow 0)
-          (iterateOnArray 0))
+          (custom-timeout)
+          (transfer-row 0)
+          (iterate-on-array 0))
         (begin
-          (resetGrid)
-          (removeAttribute (getElementById idGoBtn)
+          (reset-grid)
+          (remove-attribute (get-element-by-id id-go-btn)
                            "disabled")))))
 
-(##inline-host-statement "gameOfLife = g_scm2host(@1@);" gameOfLife)
+(##inline-host-statement "gameOfLife = g_scm2host(@1@);" game-of-life)
 
-(define (customTimeout)
+(define (custom-timeout)
   (##inline-host-statement "setTimeout(gameOfLife, 500);"))
 
-(define cellClick
+(define cell-click
   (lambda (id)
-    (if (not gameOn)
-        (let ((cell (getElementById id)))
-          (let ((oldClass (getAttribute cell "class")))
-            (setAttribute cell
+    (if (not game-on)
+        (let ((cell (get-element-by-id id)))
+          (let ((old-class (get-attribute cell "class")))
+            (set-attribute cell
                           "class"
-                          (if (or (not (isUsableObject oldClass))
+                          (if (or (not (usable-obj? old-class))
                                   (string=? ""
-                                            oldClass))
+                                            old-class))
                               "selected"
                               "")))))))
 
-(##inline-host-statement "cellClick = g_scm2host(@1@);" cellClick)
+(##inline-host-statement "cellClick = g_scm2host(@1@);" cell-click)
 
-(define (constructCells i base noROw)
-  (if (< i nbCol)
-      (let ((id (makeId i noROw)))
-        (constructCells (+ i 1)
+(define (construct-cells i base no-row)
+  (if (< i nb-col)
+      (let ((id (make-id i no-row)))
+        (construct-cells (+ i 1)
                         (string-append base
                                        "<td id='"
                                        id
                                        "' onclick='cellClick(this.id);'><div></div></td>")
-                        noROw))
+                        no-row))
       base))
 
-(define (constructRows i base)
-  (if (< i nbRow)
-      (constructRows (+ i 1)
+(define (construct-rows i base)
+  (if (< i nb-row)
+      (construct-rows (+ i 1)
                      (string-append base
                                     "<tr>"
-                                    (constructCells 0 "" i)
+                                    (construct-cells 0 "" i)
                                     "</tr>"))
       base))
 
-(define initLife
+(define init-life
   (lambda ()
-    (define (fillRow i array initValue)
-      (if (< i nbRow)
-          (fillRow (+ i 1)
+    (define (fill-row i array init-value)
+      (if (< i nb-row)
+          (fill-row (+ i 1)
                    (vector-append array
-                                  (vector initValue))
-                   initValue)
+                                  (vector init-value))
+                   init-value)
           array))
-    (define (fillArray i array initValue)
-      (if (< i nbRow)
-          (fillArray (+ i 1)
+    (define (fill-array i array init-value)
+      (if (< i nb-row)
+          (fill-array (+ i 1)
                      (vector-append array
-                                    (vector (fillRow 0 (vector) initValue)))
-                     initValue)
+                                    (vector (fill-row 0 (vector) init-value)))
+                     init-value)
           array))
-    (define (colsAround x y difCol difRow)
-      (if (<= difCol 1)
+    (define (cols-around x y dif-col dif-row)
+      (if (<= dif-col 1)
           (begin
-            (if (not (and (equal? difCol 0)
-                          (equal? difRow 0)))
-                (let ((indX (modulo (+ x difCol nbCol) nbCol))
-                      (indY (modulo (+ y difRow nbRow) nbRow)))
-                  (vector-set! (vector-ref around indY) indX
-                               (+ 1 (vector-ref (vector-ref around indY) indX)))
-                  (vector-set! (vector-ref oldAround indY) indX
-                               (+ 1 (vector-ref (vector-ref oldAround indY) indX)))))
-            (colsAround x y (+ difCol 1) difRow))))
-    (define (rowsAround x y difRow)
-      (if (<= difRow 1)
+            (if (not (and (equal? dif-col 0)
+                          (equal? dif-row 0)))
+                (let ((ind-x (modulo (+ x dif-col nb-col) nb-col))
+                      (ind-y (modulo (+ y dif-row nb-row) nb-row)))
+                  (vector-set! (vector-ref around ind-y) ind-x
+                               (+ 1 (vector-ref (vector-ref around ind-y) ind-x)))
+                  (vector-set! (vector-ref old-around ind-y) ind-x
+                               (+ 1 (vector-ref (vector-ref old-around ind-y) ind-x)))))
+            (cols-around x y (+ dif-col 1) dif-row))))
+    (define (rows-around x y dif-row)
+      (if (<= dif-row 1)
           (begin
-            (colsAround x y -1 difRow)
-            (rowsAround x y (+ difRow 1)))))
-    (define (iterateOnRow i noROw)
-      (if (< i nbCol)
+            (cols-around x y -1 dif-row)
+            (rows-around x y (+ dif-row 1)))))
+    (define (iterate-on-row i no-row)
+      (if (< i nb-col)
           (begin
-            (let ((cellTemp (getElementById (makeId i noROw))))
-              (let ((oldClass (getAttribute cellTemp "class")))
-                (let ((isAlive (and (isUsableObject oldClass)
+            (let ((cell-temp (get-element-by-id (make-id i no-row))))
+              (let ((old-class (get-attribute cell-temp "class")))
+                (let ((is-alive (and (usable-obj? old-class)
                                     (string=? "selected"
-                                              oldClass))))
+                                              old-class))))
                   (begin
-                    (setAttribute cellTemp "class" (if isAlive
+                    (set-attribute cell-temp "class" (if is-alive
                                                        "alive"
                                                        "dead"))
-                    (vector-set! (vector-ref alive noROw) i isAlive)
-                    (if isAlive
-                        (rowsAround i noROw -1))))))
-            (iterateOnRow (+ i 1) noROw))))
-    (define (iterateOnArray i)
-      (if (< i nbRow)
+                    (vector-set! (vector-ref alive no-row) i is-alive)
+                    (if is-alive
+                        (rows-around i no-row -1))))))
+            (iterate-on-row (+ i 1) no-row))))
+    (define (iterate-on-array i)
+      (if (< i nb-row)
           (begin
-            (iterateOnRow 0 i)
-            (iterateOnArray (+ i 1)))))
+            (iterate-on-row 0 i)
+            (iterate-on-array (+ i 1)))))
     (begin
-      (set! alive (fillArray 0 (vector) #f))
-      (set! around (fillArray 0 (vector) 0))
-      (set! oldAround (fillArray 0 (vector) 0))
-      (iterateOnArray 0)
-      (set! gameOn #t)
-      (customTimeout))))
+      (set! alive (fill-array 0 (vector) #f))
+      (set! around (fill-array 0 (vector) 0))
+      (set! old-around (fill-array 0 (vector) 0))
+      (iterate-on-array 0)
+      (set! game-on #t)
+      (custom-timeout))))
 
-(define startLife
+(define start-life
   (lambda ()
-    (setAttribute (getElementById idGoBtn)
+    (set-attribute (get-element-by-id id-go-btn)
                   "disabled"
                   "true")
-    (let ((stopBtn (getElementById idStopBtn)))
-      (if (hasAttribute stopBtn "disabled")
-          (removeAttribute stopBtn "disabled")))
-    (initLife)))
+    (let ((stop-btn (get-element-by-id id-stop-btn)))
+      (if (has-attribute stop-btn "disabled")
+          (remove-attribute stop-btn "disabled")))
+    (init-life)))
 
-(##inline-host-statement "startLife = g_scm2host(@1@);" startLife)
+(##inline-host-statement "startLife = g_scm2host(@1@);" start-life)
 
-(define stopLife
+(define stop-life
   (lambda ()
-    (setAttribute (getElementById idStopBtn)
+    (set-attribute (get-element-by-id id-stop-btn)
                   "disabled"
                   "true")
-    (let ((goBtn (getElementById idGoBtn)))
-      (if (hasAttribute goBtn "disabled")
-          (removeAttribute goBtn "disabled")))
-    (set! gameOn #f)))
+    (let ((go-btn (get-element-by-id id-go-btn)))
+      (if (has-attribute go-btn "disabled")
+          (remove-attribute go-btn "disabled")))
+    (set! game-on #f)))
 
-(##inline-host-statement "stopLife = g_scm2host(@1@);" stopLife)
+(##inline-host-statement "stopLife = g_scm2host(@1@);" stop-life)
 
 (begin
   (##inline-host-statement "document.title = 'Game of Life | Scheme to Javascript';")
-  (appendHTML (querySelector "head")
+  (append-html (query-selector "head")
               (<style> type: "text/css"
                        (string-append "* {"
                                       "border: 0;"
@@ -245,7 +245,7 @@
                                         ; "border-spacing: 5px;"
                                       "margin: 10px auto;"
                                       "padding: 10px;"
-                                      "width: " (number->string (* nbCol 25)) "px;"
+                                      "width: " (number->string (* nb-col 25)) "px;"
                                       "}"
                                       "tr {"
                                       "height: 25px;"
@@ -300,13 +300,13 @@
                                       "cursor: pointer;"
                                       "}")))
   (document.write (<table>))
-  (setInnerHTML (querySelector "table")
-                (constructRows 0 ""))
-  (document.write (<button> id: idGoBtn
+  (set-inner-html (query-selector "table")
+                (construct-rows 0 ""))
+  (document.write (<button> id: id-go-btn
                             onclick: "startLife()"
                             type: "button"
                             "GO!"))
-  (document.write (<button> id: idStopBtn
+  (document.write (<button> id: id-stop-btn
                             disabled:
                             onclick: "stopLife()"
                             type: "button"
